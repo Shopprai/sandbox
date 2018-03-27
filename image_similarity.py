25be0b9c086782ba8da99640a1b3214c58c54795
Peter Lu <6peterlu@gmail.com>
2018-03-26 21:24:36 -0700

    image scraping scrapes all images on any input url

--- image_similarity.py
+++ image_similarity.py
@@ -14,11 +14,7 @@ from lxml import html, etree
 # API KEY: dbe3f2920953470199637703b6566c31
 # TODO: figure out how to download all images
 
-# img1 = ClImage(url="https://samples.clarifai.com/metro-north.jpg", image_id="train1")
-# img2 = ClImage(url="https://samples.clarifai.com/puppy.jpeg", image_id="puppy1")
 app = ClarifaiApp(api_key='dbe3f2920953470199637703b6566c31')
-
-# app.inputs.bulk_create_images([img1, img2])
 
 def upload_images(image_urls, batch_size=30):
 	image_list = []
@@ -27,10 +23,11 @@ def upload_images(image_urls, batch_size=30):
 		image_list.append(ClImage(url = image_url))
 		image_counter += 1
 		if image_counter % batch_size == 0:
+			print("batch submitted", sys.stdout)
 			app.inputs.bulk_create_images(image_list)
 			image_list = []
 	app.inputs.bulk_create_images(image_list)
-	print("upload_images: complete")
+	print("upload_images: complete", sys.stdout)
 
 def search_by_filename(filename):
 # metro url: https://www.thenational.ae/image/policy:1.286039:1499453255/image/jpeg.jpg?f=16x9&w=1200&$p$f$w=dfa40e8
@@ -44,7 +41,6 @@ def search_by_url(url):
 	search = app.inputs.search_by_image(url=url)
 	search_results = []
 	for search_result in search:
-		print(vars(search_result), file=sys.stdout)
 		search_results.append({"score": search_result.score, "url": search_result.url})
 	return search_results
 
@@ -56,13 +52,8 @@ def get_image_urls(url, request_type="GET"):
 		src_file = open("download.html", "wb")
 		page = requests.get(url)
 		tree = html.fromstring(page.content)
-		# image_urls = tree.xpath('//*[@class="_2oHs74P"]/a/div[1]/img/@src')
-		image_urls = tree.xpath('//*[@class="_1FN5N-P"]/img/@src')
-		# divs = tree.xpath('//*[@class="_1FN5N-P"]')
-		# print(len(divs))
-		# for div in divs:
-		# 	print(etree.tostring(div, pretty_print=True))
-		print("get_image_urls: complete")
+		image_urls = tree.xpath('//img/@src') # generalizes to all images on page, even non clothing ones.
+		print("get_image_urls: complete", sys.stdout)
 		return image_urls
 
 # image_urls = get_image_urls("http://www.asos.com/women/ctas/fashion-trends-styling-3/cat/?cid=16264&ctaref=shop%7Coccasionwearww%7Cww_hp_2&page=1")
