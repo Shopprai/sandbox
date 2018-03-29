7e6130ad77a9a92cb85ef6661058f9047d3387d5
Peter Lu <6peterlu@gmail.com>
2018-03-28 22:42:04 -0700

    ah there it is

--- schema.py
+++ schema.py
@@ -1,7 +1,7 @@
 from pymodm import fields, MongoModel, connect
 import os
 
-MONGO_URL = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/shopprai')
+MONGO_URL = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/shopprai')
 print(MONGO_URL)
 
 connect(MONGO_URL, alias="main")
